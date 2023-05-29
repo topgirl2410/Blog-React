1ae14cd42ae4d18f4695e2b77a6bead606d23e13
topgirl2410 <maria.topgirl82@gmail.com>
2023-05-29 19:39:45 +0200

    comenzando mi blog

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,43 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/write",
+    element: <Write />
+  },
+  {
+    path: "/single",
+    element: <Single />
+  }
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return <div>
+    <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
