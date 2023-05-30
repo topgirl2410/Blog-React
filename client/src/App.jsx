4e647b0ae31916f11d47acbd9c9730d326a04254
topgirl2410 <maria.topgirl82@gmail.com>
2023-05-31 01:46:04 +0200

    creando registro e inicio de sesion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,34 +9,55 @@ import Login from "./pages/Login";
 import Write from "./pages/Write";
 import Home from "./pages/Home";
 import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./style.scss";
 
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
   {
     path: "/register",
-    element: <Register />
+    element: <Register />,
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
   },
-  {
-    path: "/write",
-    element: <Write />
-  },
-  {
-    path: "/single",
-    element: <Single />
-  }
 ]);
 
 function App() {
-  return <div>
-    <RouterProvider router={router} />
+  return <div className="app">
+    <div className="container">
+      <RouterProvider router={router} />
+    </div>
   </div>;
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div className='auth center'>
+            <h1>Register</h1>
+            <form>
+                <input type='text' placeholder='username' />
+                <input type='email' placeholder='email' />
+                <input type='password' placeholder='password' />
+                <button>Register</button>
+                <p>This is an error !!</p>
+                <span>Don you have an account?? <Link to='/login'>Login</Link> </span>
+            </form>
+        </div>
     )
 }
 
