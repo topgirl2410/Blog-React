011f88bd9a1daedf0d3ba73111afb11feed09d73
topgirl2410 <maria.topgirl82@gmail.com>
2023-05-31 01:50:29 +0200

    cambiando el color del boton y h1

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
-        <div className='auth center'>
+        <div className='auth'>
             <h1>Login</h1>
             <form>
-                <input type='text' placeholder='username' />
-                <input type='password' placeholder='password' />
+                <input required type='text' placeholder='username' />
+                <input required type='password' placeholder='password' />
                 <button>Login</button>
                 <p>This is an error !!</p>
                 <span>Don't you have an account?? <Link to='/register'>Register</Link> </span>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Register = () => {
     return (
-        <div className='auth center'>
+        <div className='auth'>
             <h1>Register</h1>
             <form>
-                <input type='text' placeholder='username' />
-                <input type='email' placeholder='email' />
-                <input type='password' placeholder='password' />
+                <input required type='text' placeholder='username' />
+                <input required type='email' placeholder='email' />
+                <input required type='password' placeholder='password' />
                 <button>Register</button>
                 <p>This is an error !!</p>
                 <span>Don you have an account?? <Link to='/login'>Login</Link> </span>
