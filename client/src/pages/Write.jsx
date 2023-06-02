09ab3511b312073667edc4415ca171d339555c5f
topgirl2410 <maria.topgirl82@gmail.com>
2023-06-02 16:17:48 +0200

    write acabada

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,12 +1,68 @@
 import React from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react'
 
 const Write = () => {
+    const [value, setValue] = useState('');
+
+
     return (
-        <div className='write'>
-            <div className="content">content</div>
+        <div className='add'>
+            <div className="content">
+                <input type="text" placeholder='Title' />
+                <div className="editorContainer">
+                    <ReactQuill className='editor' theme="snow" value={value} onChange={setValue} />
+                </div>
+            </div>
             <div className="menu">
-                <div className="item">1i</div>
-                <div className="item">i2</div>
+                <div className="item">
+                    <h1>Publish</h1>
+                    <span>
+                        <b>Status: </b> Draft
+                    </span>
+                    <span>
+                        <b>Visibility: </b> Public
+                    </span>
+                    <input style={{ display: "none" }} type="file" id='file' />
+                    <label className='file' htmlFor="file">Upload Image</label>
+                    <div className="buttons">
+                        <button >Save as a draft</button>
+                        <button >Publish</button>
+                    </div>
+                </div>
+                <div className="item">
+                    <h1>Category</h1>
+                    <div className="cat">
+                        <input type="radio" name='cat' value="art" id='art' />
+                        <label htmlFor="art">Art</label>
+                    </div>
+
+                    <div className="cat">
+                        <input type="radio" name='cat' value="science" id='science' />
+                        <label htmlFor="art">Science</label>
+                    </div>
+
+                    <div className="cat">
+                        <input type="radio" name='cat' value="technology" id='technology' />
+                        <label htmlFor="art">Technology</label>
+                    </div>
+
+                    <div className="cat">
+                        <input type="radio" name='cat' value="cinema" id='cinema' />
+                        <label htmlFor="art">Cinema</label>
+                    </div>
+
+                    <div className="cat">
+                        <input type="radio" name='cat' value="design" id='design' />
+                        <label htmlFor="art">Design</label>
+                    </div>
+
+                    <div className="cat">
+                        <input type="radio" name='cat' value="food" id='food' />
+                        <label htmlFor="art">Food</label>
+                    </div>
+                </div>
             </div>
         </div>
     )
